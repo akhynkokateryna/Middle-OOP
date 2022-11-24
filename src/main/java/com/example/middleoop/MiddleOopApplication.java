cc48a94938d83d76aabdad20080339f9d04239a6
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-11-24 15:04:40 +0200

    Initial commit

---
+++ src/main/java/com/example/middleoop/MiddleOopApplication.java
@@ -0,0 +1,13 @@
+package com.example.middleoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/middleoop/MiddleOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.middleoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
