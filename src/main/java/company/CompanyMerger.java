70aceccf3f9328d307902771b93623dff20cc762
Dmytro Vasylkiv <67468470+Dima-Vas@users.noreply.github.com>
2022-11-26 18:09:36 +0200

    Merger uploaded
    List<Company> => JSONObject

---
+++ src/main/java/company/CompanyMerger.java
@@ -0,0 +1,67 @@
+package company;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class CompanyMerger {
+    CompanyMerger instance = null;
+
+    private CompanyMerger getInstance() {
+        if (instance == null) {
+            return new CompanyMerger();
+        }
+        return instance;
+    }
+
+    public JSONObject mergeIntoJSON(List<Company> toScrape) {
+        JSONObject output = new JSONObject();
+
+        HashMap<String, List<String>> valuesScraped = new HashMap<String, List<String>>();
+
+        String[] tokenList = {"name", "twitter", "facebook", "logo", "icon", "employees", "address"};
+
+        for (String token:
+             tokenList) {
+            valuesScraped.put(token, new ArrayList<>());
+        }
+
+        Field[] props = Company.class.getDeclaredFields();
+
+        for (Field yo : props) {
+            for (int i = 0; i < toScrape.size(); i++) {
+                try {
+                    String attributeValue = String.valueOf(yo.get(toScrape.get(i)));
+                    Object attributeName = yo.getName();
+                    if (!valuesScraped.get(attributeName).contains(attributeValue) && !attributeValue.equals("null")) {
+                        valuesScraped.get(attributeName).add(attributeValue);
+                    }
+                }
+                catch (JSONException e) {
+
+                }
+                catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        for (String token : tokenList) {
+            try {
+                String toWrite = valuesScraped.get(token).get(0);
+                output.put(token, toWrite);
+            }
+            catch (IndexOutOfBoundsException e) {
+                output.put(token, "");
+            }
+        }
+
+        return output;
+    }
+
+}
